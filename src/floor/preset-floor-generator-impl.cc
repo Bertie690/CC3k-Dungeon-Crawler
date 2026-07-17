a5f457be4c0ceca1fcf75f0ac5a52e15efd6ace2
liamdty <liamdty@gmail.com>
2026-07-17 15:21:56 -0400

    Add stubbed placePresetEntitites waiting on factories

--- src/floor/preset-floor-generator-impl.cc
+++ src/floor/preset-floor-generator-impl.cc
@@ -4,6 +4,8 @@ module presetfloorgenerator;
 #include <fstream>
 #include <string>
 #include <vector>
+
+#include "../entities/staircase.cc"
 #include "floor.cc"
 #include "preset-floor-generator.cc"
 #else
@@ -11,6 +13,7 @@ import <fstream>;
 import <string>;
 import <vector>;
 import floor;
+import staircase;
 #endif  // __INTELLISENSE__
 
 using namespace std;
@@ -27,12 +30,53 @@ vector<string> readFloorLines(ifstream& input) {
 
 PresetFloorGenerator::PresetFloorGenerator(RNG& rng, const string& fileName) : rng{rng}, input{fileName} {}
 
+void placePresetEntities(Floor& floor, const vector<string>& floorLines) {
+  for (int y = 0; y < Floor::HEIGHT; ++y) {
+    for (int x = 0; x < Floor::WIDTH; ++x) {
+      const char c = floorLines[y][x];
+      const Position position{x, y};
+      switch (c) {
+        case '\\':
+          floor.getCell(position).add(make_shared<Staircase>(position));
+          break;
+        case '@':
+          // TODO: place player
+          break;
+        case 'H':
+        case 'W':
+        case 'E':
+        case 'O':
+        case 'M':
+        case 'D':
+        case 'L':
+          // TODO: create enemy: EnemyFactory
+          break;
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+          // TODO: create potion: PotionFactory
+          break;
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+          // TODO: create gold : GoldFactory
+          break;
+      }
+    }
+  }
+}
+
 Floor PresetFloorGenerator::generateFloor() {
   vector<string> floorLines = readFloorLines(input);
   ++nextFloor;
 
   Floor floor = createBaseFloor(rng);
 
-  // TODO: place @, enemies, potions, gold, stairs
+  // TODO: add player and factories to signature
+  placePresetEntities(floor, floorLines);
   return floor;
 }
